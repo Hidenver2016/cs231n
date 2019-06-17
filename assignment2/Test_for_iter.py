f7e3a4e9bb888f1dcc68d1be4eacd078e85e14f4
Hidenver2016 <hgjiang2013@live.com>
2019-06-17 11:32:32 -0600

    Add files via upload

---
+++ assignment2/Test_for_iter.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 11:48:59 2018
+
+@author: hjiang
+"""
+
+class Fib(object):
+    def __init__(self, max):
+        super(Fib, self).__init__()
+        self.max = max
+
+    def __iter__(self):
+        self.a = 0
+        self.b = 1
+        return self
+
+    def __next__(self):
+        fib = self.a
+        if fib > self.max:
+            raise StopIteration
+        self.a, self.b = self.b, self.a + self.b
+        return fib
+
+def main():
+    fib = Fib(100)
+    for i in fib:
+        print(i)
+
+if __name__ == '__main__':
+    main()
